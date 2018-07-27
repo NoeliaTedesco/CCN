20a2cfb124c99bc912066973d1e9088f5a0b2218
NoeliaTedesco <33137554+NoeliaTedesco@users.noreply.github.com>
2018-07-27 12:19:43 -0300

    Se suben los cambios en las clases busqueda_Prestaciones

--- src/test/java/ar/com/osde/genesisCobertura/selenium/Busq_Prestaciones_Medicamentos.java
+++ src/test/java/ar/com/osde/genesisCobertura/selenium/Busq_Prestaciones_Medicamentos.java
@@ -1,72 +1,99 @@
 package ar.com.osde.genesisCobertura.selenium;
 
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.util.List;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
-
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Busq_Prestaciones_Medicamentos {
-	
-	@CacheLookup
-	@FindBy(id = "resultadoPrestacion")	    
-	private WebElement ResultadoPrestaciones;
-	@FindBy(css = "div.itemView")	    
-	private WebElement medicamentos;
 
-	@FindBy(className = "ui-autocomplete-input")	    
+	private WebDriver driver;
+	private WebDriverWait wait;
+	private Logger log;
+
+	@CacheLookup
+	@FindBy(id = "resultadoPrestacion")
+	private WebElement ResultadoPrestaciones;
+	@FindBy(css = "div.itemView")
+	private WebElement medicamentos;
+	@FindBy(xpath = "//input[contains(@class, 'ui-autocomplete-input']")
 	private WebElement autocompletePrestacion;
-	@FindBy(css = "select.validate.ajaxComboBox")	    
+	@FindBy(css = "select.validate.ajaxComboBox")
 	private WebElement comboContexto;
-	@FindBy(xpath = "//img[contains(@src, 'http://concobergent.intranet.osde:9380/ccn-frontend/images/usabilidad/iconos_on/plus.png')]")    
+	@FindBy(xpath = "//img[contains(@src, 'http://concobergent.intranet.osde:9380/ccn-frontend/images/usabilidad/iconos_on/plus.png')]")
 	private WebElement imagenAgregar;
-	@FindBy(xpath = "//img[contains(@src, 'http://concobergent.intranet.osde:9380/ccn-frontend/images/limpiar.png')]")    
+	@FindBy(xpath = "//img[contains(@src, 'http://concobergent.intranet.osde:9380/ccn-frontend/images/limpiar.png')]")
 	private WebElement imagenBorrar;
-	@FindBy(xpath = "//div[contains(@textContent, 'Domicilio')]")	    
+	@FindBy(xpath = "//div[contains(@textContent, 'Domicilio')]")
 	private WebElement filtroDomicilio;
-	@FindBy(xpath = "//img[contains(@src, 'http://concobergent.intranet.osde:9380/ccn-frontend/images/usabilidad/iconos_on/editar-on.gif')]")    
+	@FindBy(xpath = "//img[contains(@src, 'http://concobergent.intranet.osde:9380/ccn-frontend/images/usabilidad/iconos_on/editar-on.gif')]")
 	private WebElement imagenEditar;
-	@FindBy(xpath = "//div[contains(@textContent, 'Zona')]")	    
+	@FindBy(xpath = "//div[contains(@textContent, 'Zona')]")
 	private WebElement filtroZona;
-	@FindBy(xpath = "//div[contains(@title, 'Filial')]")	    
+	@FindBy(xpath = "//div[contains(@title, 'Filial')]")
 	private WebElement comboFilial;
-	@FindBy(xpath = "//div[contains(@title, 'Zona')]")	    
+	@FindBy(xpath = "//div[contains(@title, 'Zona')]")
 	private WebElement comboZona;
-	@FindBy(css = "select.subzona.notNull.validate.ajaxComboBox")	    
+	@FindBy(css = "select.subzona.notNull.validate.ajaxComboBox")
 	private WebElement comboSubZona;
-	@FindBy(xpath = "//div[contains(@textContent, 'Prestador')]")	    
+	@FindBy(xpath = "//div[contains(@textContent, 'Prestador')]")
 	private WebElement filtroPrestador;
-	@FindBy(css = "select.filial.ajaxComboBox")	    
+	@FindBy(css = "select.filial.ajaxComboBox")
 	private WebElement comboFilialPrestador;
-	@FindBy(id = "prestador")	    
+	@FindBy(id = "prestador")
 	private WebElement autocompletePrestador;
-	@FindBy(id = "btnBuscar")	    
+	@FindBy(id = "btnBuscar")
 	private WebElement btnBuscar;
-	@FindBy(id = "btnLimpiar")	    
+	@FindBy(id = "btnLimpiar")
 	private WebElement btnLimpiar;
-	
-	
-	public boolean cargarPrestacion(String busqueda) throws AWTException {
-		autocompletePrestacion.click();
-		autocompletePrestacion.sendKeys(busqueda);
-		Robot rw = new Robot();
-		rw.keyPress(KeyEvent.VK_DOWN);
-		rw.keyRelease(KeyEvent.VK_DOWN);
-		rw.keyPress(KeyEvent.VK_ENTER);
-		rw.keyRelease(KeyEvent.VK_ENTER);
-		
+
+	public Busq_Prestaciones_Medicamentos(WebDriver driver) {
+		this.driver = driver;
+		wait = new WebDriverWait(this.driver, 30);
+		PageFactory.initElements(driver, this);
+		log = Logger.getLogger(getClass().getName());
+	}
+
+	public void cargarPrestacion(String busqueda) throws AWTException {
+		List<WebElement> lstRegistros = autocompletePrestacion.findElements(By.className("elemento input-prestacion validate params"));
+		for (WebElement registros : lstRegistros) {
+			List<WebElement> registro  = registros.findElements(By.className("ui-autocomplete-input"));
+			for(WebElement autocomplete : registro) {
+				autocomplete.click();
+				autocomplete.sendKeys(busqueda);
+				autocomplete.submit();
+				
+	/*			Robot rw = new Robot();
+				rw.keyPress(KeyEvent.VK_DOWN);
+				rw.keyRelease(KeyEvent.VK_DOWN);
+				rw.keyPress(KeyEvent.VK_ENTER);
+				rw.keyRelease(KeyEvent.VK_ENTER);
+*/
+			}
+		}
+
+	}
+
+	public boolean validarValorCampo(String busqueda) throws AWTException {
+
 		String prestacionSeleccionada = autocompletePrestacion.getText().toString();
 
-		if (prestacionSeleccionada == busqueda){
-		return true;
+		if (prestacionSeleccionada == busqueda) {
+			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 }
--- src/test/java/ar/com/osde/genesisCobertura/testng/Test_Busqueda_Prestaciones.java
+++ src/test/java/ar/com/osde/genesisCobertura/testng/Test_Busqueda_Prestaciones.java
@@ -1,5 +1,7 @@
 package ar.com.osde.genesisCobertura.testng;
 
+import java.awt.AWTException;
+
 import org.apache.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -11,50 +13,46 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import ar.com.osde.genesisCobertura.selenium.Busq_Prestaciones_Medicamentos;
 import ar.com.osde.portalsua.selenium.Base_Test;
 import ar.com.osde.portalsua.selenium.Busqueda_Multisujeto;
-import ar.com.osde.portalsua.selenium.Consola_Recepcion;
+
 import ar.com.osde.portalsua.selenium.Intranet_Login;
 import ar.com.osde.portalsua.selenium.Intranet_Pagina_Principal;
-import ar.com.osde.portalsua.selenium.Modo_Usuario;
+
 import ar.com.osde.portalsua.selenium.PortalSua_Pagina_Principal;
 import ar.com.osde.portalsua.selenium.Servicio_Sujeto;
 import ar.com.osde.portalsua.selenium.Widget_Sujeto_Contacto;
 
 public class Test_Busqueda_Prestaciones {
-	
 
 	private Busqueda_Multisujeto buscador;
-	private Consola_Recepcion consola;
-	private Modo_Usuario modoUsuario;
 	private Logger log;
 	private Base_Test driver;
-    private WebDriver robot;
-	
-	public Test_Busqueda_Prestaciones() {
-		
-	}
+	private WebDriver robot;
+	private Busq_Prestaciones_Medicamentos busqPrestacion;
 
-	/* PRECONDICIONES:
-	 *  				Usuario con b�squeda paralela desactivado,
-	 * 					Usuario con permisos para recepcionar/atender todos los sectores
-	 * 					Usuario logueado a filial/Cap Metro-Metro
-	 * 					La filial/cap del usuario logueado debe tener recepcion de sujetos activados
+	/*
+	 * PRECONDICIONES: Usuario con b�squeda paralela desactivado, Usuario con
+	 * permisos para recepcionar/atender todos los sectores Usuario logueado a
+	 * filial/Cap Metro-Metro La filial/cap del usuario logueado debe tener
+	 * recepcion de sujetos activados
 	 */
-	
-	@Parameters({"browser", "ipNodo","usuario","password","ambiente", "sSujetoAnular","appGerencia"})
+
+	@Parameters({ "browser", "ipNodo", "usuario", "password", "ambiente", "appGerencia" })
 	@BeforeClass
-	public void startUp(@Optional("iexplorer") String browser, String ipNodo, String usuario, String password, String ambiente, String sSujetoAnular, String appName, ITestContext ctx) throws InterruptedException {
-		//Se espera un tiempo para cerrar el explorer anterior
+	public void startUp(@Optional("iexplorer") String browser, String ipNodo, String usuario, String password,
+			String ambiente, String appName, ITestContext ctx) throws InterruptedException {
+		// Se espera un tiempo para cerrar el explorer anterior
 		Thread.sleep(3000);
 		log = Logger.getLogger(getClass().getName());
 		if (log.isInfoEnabled()) {
 			log.info("************** INICIA SUITE DE PRUEBA **************");
 			log.info("Suite: " + ctx.getCurrentXmlTest().getSuite().getName());
-			log.info("Inicia StartUp");			
+			log.info("Inicia StartUp");
 		}
-		//Se abre el browser
-		driver = new Base_Test(browser, ipNodo); 
+		// Se abre el browser
+		driver = new Base_Test(browser, ipNodo);
 		// Creamos un objeto de tipo Login_Intranet
 		Intranet_Login loginIntra = new Intranet_Login(driver.getDriver());
 		// Hacemos el Login Completo
@@ -62,85 +60,59 @@ public class Test_Busqueda_Prestaciones {
 		// Creamos un objeto de tipo Intranet
 		Intranet_Pagina_Principal intranet = new Intranet_Pagina_Principal(driver.getDriver());
 		// Abrimos Portal desde integra
-		intranet.AbrirAplicacion(appName);		
+		intranet.AbrirAplicacion(appName);
 		// Creamos un objeto de tipo Portal SUA
 		PortalSua_Pagina_Principal portalSUA = new PortalSua_Pagina_Principal(driver.getDriver());
-		//Abrimos Portal en el ambiente que corresponda
+		// Abrimos Portal en el ambiente que corresponda
 		portalSUA.abrirPortal(ambiente);
 		// Comprobamos que portal se haya cargado
-		if(!portalSUA.seMuestraPortal()) portalSUA.recargarPortal();		
-		Assert.assertTrue(portalSUA.seMuestraPortal());		
+		if (!portalSUA.seMuestraPortal())
+			portalSUA.recargarPortal();
+		Assert.assertTrue(portalSUA.seMuestraPortal());
 		// Creamos un objeto de tipo Login_Portal
-		buscador = new Busqueda_Multisujeto(driver.getDriver()); 
-		// Creamos un objeto de tipo Modo Usuario
-		modoUsuario = new Modo_Usuario(driver.getDriver());		
-		// Creamos un objeto de tipo Consola Recepcion
-		consola = new Consola_Recepcion(driver.getDriver());		
-		Thread.sleep(5000);
-		consola.irConsolaRecepcion();
-		// habilitamos el modo recepcion
-		modoUsuario.habilitar_modo_recepcion();
-		//Si encuentra el socio en consola lo anula				
-		consola.anularSujeto(sSujetoAnular);
+		buscador = new Busqueda_Multisujeto(driver.getDriver());
 	}
-	
-	
-	@Parameters({"nombreCarpetaServicio", "nombreSubCarpetaServicio", "idServicio", "sSocio"})
-	@BeforeMethod 
-	public void beforeMethod(String nombreCarpetaServicio, String nombreSubCarpetaServicio, String idServicio, String sSujeto) throws InterruptedException {
-		Thread.sleep(1000);    	    			
+
+	@Parameters({ "nombreCarpetaServicio", "nombreSubCarpetaServicio", "nombreServicio", "sSocio", "idServicio"})
+	@BeforeMethod
+	public void beforeMethod(String nombreCarpetaServicio, String nombreSubCarpetaServicio, String nombreServicio,
+		String sSujeto, String idServicio) throws InterruptedException {
+		Thread.sleep(9000);
 		// Se Busca el sujeto
-		buscador.buscarSujeto(sSujeto);    	    	
+		buscador.buscarSujeto(sSujeto);
 		// Se crea un objeto de tipo Caracteristica_Sujeto
 		Widget_Sujeto_Contacto wdgCaracteristica = new Widget_Sujeto_Contacto(driver.getDriver());
 		// Se valida que este visible el widget de caracteristicas
 		Assert.assertTrue(wdgCaracteristica.esVisibleCaracteristica());
 		Servicio_Sujeto wdg_ServiciosSujetos = new Servicio_Sujeto(driver.getDriver());
-		//Expandimos el panel izquierdo
+		// Expandimos el panel izquierdo
 		wdg_ServiciosSujetos.expandirPanelIzquierdo();
 		// Validamos que este visible el widget de busqueda de servicios de sujetos
 		Assert.assertTrue(wdg_ServiciosSujetos.esVisibleWidgetBuscadorServiciosSujeto());
-		//Abrimos el arbol de servicios del sujeto
+		// Abrimos el arbol de servicios del sujeto
 		wdg_ServiciosSujetos.abrirArbolServicio();
-		//Abrimos la carpeta del arbol de servicios del sujeto		
+		// Abrimos la carpeta del arbol de servicios del sujeto
 		wdg_ServiciosSujetos.expandirCarpetaArbolServicio(nombreCarpetaServicio, nombreSubCarpetaServicio);
 		robot = driver.getDriver();
 		JavascriptExecutor js = ((JavascriptExecutor) robot);
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 		Thread.sleep(3000);
-		//Abrimos el arbol de servicios del sujeto
+		// Abrimos el arbol de servicios del sujeto
 		Assert.assertTrue(wdg_ServiciosSujetos.esVisibleServicio(idServicio));
-		wdg_ServiciosSujetos.abrirServicio(idServicio);
-		Thread.sleep(9000);	
+		wdg_ServiciosSujetos.abrirServicio(nombreServicio);
+		Thread.sleep(10000);
 		Assert.assertTrue(wdg_ServiciosSujetos.esVisibleWidgetServiciosSujeto(idServicio));
 	}
-	
-	@Parameters({"sSocio","idServicio","nombreCarpetaServicio","nombreSubCarpetaServicio"})
+
+	@Parameters({ "CodPrestacion", "DesPrestacion"})
 	@Test
-	public void abrirServicioSocio(String idSocio, String idServicio, String nombreCarpetaServicio, String nombreSubCarpetaServicio) throws InterruptedException {
-		
-		// Buscamos el socio con el indicador de observaciones de socio
-		buscador.buscarSujeto(idSocio);    	    	
-		//Creamos un objeto de tipo Caracteristica_Sujeto
-		Widget_Sujeto_Contacto wdg_caracteristica = new Widget_Sujeto_Contacto(driver.getDriver());
-		// Validamos que este visible el widget de caracteristicas
-		Assert.assertTrue(wdg_caracteristica.esVisibleCaracteristica());    	
-		Servicio_Sujeto wdg_ServiciosSujetos = new Servicio_Sujeto(driver.getDriver());
-		//Expandimos el panel izquierdo
-		wdg_ServiciosSujetos.expandirPanelIzquierdo();
-		// Validamos que este visible el widget de busqueda de servicios de sujetos
-		Assert.assertTrue(wdg_ServiciosSujetos.esVisibleWidgetBuscadorServiciosSujeto());
-		//Abrimos el arbol de servicios del sujeto
-		wdg_ServiciosSujetos.abrirArbolServicio();
-		//Abrimos la carpeta del arbol de servicios del sujeto		
-		wdg_ServiciosSujetos.expandirCarpetaArbolServicio(nombreCarpetaServicio, nombreSubCarpetaServicio);
-		//Abrimos el arbol de servicios del sujeto
-		wdg_ServiciosSujetos.abrirServicio(idServicio);
-		Thread.sleep(9000);	
-		Assert.assertTrue(wdg_ServiciosSujetos.esVisibleWidgetServiciosSujeto(idServicio));
-		// Finalizamos el contacto
-		wdg_caracteristica.finalizarContacto();
+	public void BusquedaPrestacion(String codPrestacion, String DesPrestacion ) throws AWTException, java.lang.InterruptedException {
+
+		// Se crea un objeto para buscar la prestacion
+		busqPrestacion = new Busq_Prestaciones_Medicamentos(driver.getDriver());
+		busqPrestacion.cargarPrestacion(DesPrestacion);
+		Thread.sleep(3000);
+		Assert.assertTrue(busqPrestacion.validarValorCampo(DesPrestacion));
 	}
-	
 
 }
